8e2e3173ee408ccbd57626c542d272bb5a969bab
Allison <108605626+allihb@users.noreply.github.com>
2022-09-13 02:43:49 -0500

    Now we have many messages

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! How are you?");
+Console.WriteLine("Hello, C# Academy! How are you?");
+Console.WriteLine("Hello, C# Academy! How are you?");
+Console.WriteLine("Hello, C# Academy! How are you?");
+Console.WriteLine("Hello, C# Academy! How are you?");
+Console.WriteLine("Hello, C# Academy! How are you?");
