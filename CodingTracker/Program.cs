6644f4b76edabf0c432ad34596deead8e4b39f53
Allison Belk <108605626+allihb@users.noreply.github.com>
2022-09-13 02:46:02 -0500

    Keeping one message

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! How are you?");
-Console.WriteLine("Hello, C# Academy! How are you?");
-Console.WriteLine("Hello, C# Academy! How are you?");
-Console.WriteLine("Hello, C# Academy! How are you?");
-Console.WriteLine("Hello, C# Academy! How are you?");
-Console.WriteLine("Hello, C# Academy! How are you?");
